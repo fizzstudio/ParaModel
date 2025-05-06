e4fe504e3d7adc157dd9a96da9c6ad7e25753226
simonvarey <simonvarey@gmail.com>
2025-05-06 11:35:23 -0400

    Exported Box

--- lib/index.ts
+++ lib/index.ts
@@ -2,3 +2,4 @@
 export { DataPoint, XYDatapoint, Series, Model, facetsFromDataset, modelFromAllSeriesData, 
   modelFromManifest } from './model';
 export { strToId, enumerate, arrayEqualsBy } from './utils';
+export { Box } from './dataframe/box';
