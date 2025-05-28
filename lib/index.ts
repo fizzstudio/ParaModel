ff892044e959bc4fcf829c1545c5c106014897e5
simonvarey <simonvarey@gmail.com>
2025-05-28 13:19:16 -0400

    Export Category enum

--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,3 @@
-
 export { Model, facetsFromDataset, modelFromInlineData, modelFromExternalData } from './model/model';
 export { DataPoint, XYDatapoint, Series, XYSeries } from './model/series';
 export { strToId, enumerate, arrayEqualsBy, type AxisOrientation } from './utils';
@@ -6,3 +5,4 @@ export { Box } from './dataframe/box';
 export type { FacetSignature } from './dataframe/dataframe';
 export { type CalendarPeriod, parseCalendar, calendarGoBack, calendarEquals, calendarGoForward, calendarNumber } from './calendar_period';
 export type { Intersection, TrackingGroup } from './metadata/pair_analyzer_interface';
+export { Category } from './metadata/series_analyzer_interface';
