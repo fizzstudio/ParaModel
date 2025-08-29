79df49ced641707b36f9f2f5ee63a909551a8168
simonvarey <simonvarey@gmail.com>
2025-08-29 12:19:28 -0400

    Removed temp debug messages

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -6,6 +6,5 @@ import { Temporal } from 'temporal-polyfill';
 test('utcTimestampToPlainDateTime', () => {
   const actual = utcTimestampToPlainDateTime(1199145600000);
   const expected = Temporal.PlainDateTime.from('2008-01-01T00:00:00');
-  console.log('intest', expected.toString())
   expect(Temporal.PlainDateTime.compare(actual, expected)).toBe(0);
 })
