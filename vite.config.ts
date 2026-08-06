fd2de17aa90a092f8bea719fdd5b2f9be80bd74b
simonvarey <simonvarey@gmail.com>
2026-08-06 15:08:35 -0400

    Fixed Vitest port for Windows

--- vite.config.ts
+++ vite.config.ts
@@ -21,8 +21,16 @@ export default defineConfig({
   plugins: [],
   test: {
     browser: {
+      api: {
+        port: 5175,
+      },
       enabled: true,
-      name: 'chrome',
+      instances: [
+        {
+          browser: 'chrome',
+          headless: true
+        }
+      ]
       //headless: true, // set to false to watch in a real browser window
     }
   }
